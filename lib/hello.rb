16f7cc0fbc60154d18d0743ba81475fb069fefd4
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 19:47:07 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,7 +12,12 @@ else
   end
 end
 
-# call your method here!
+# ["Tim", "Tom", "Jim"].each do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
+# # call your method here!
 
 
 
