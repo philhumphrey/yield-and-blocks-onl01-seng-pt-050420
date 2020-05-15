85a449119a9462a69898f5ff3cab86e4de523ffb
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-15 01:25:47 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,53 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+i = 0
 
+while i < array.length
+yield(array [i])
+i = i + 1
+  end
+  array
+else
+  puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# names = ["Tim", "Tom", "Jim"]
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#   yield array[i]
+#   i =i + 1
+# names.each do |name|
+#   puts "#{name}"
+# end
+# end
+
+# other_names = []
+#   ]def goodbye_t(array)
+  
+# end
+
+
+
+
+
+
+
 
